905774d348d6f0fa62c9fcb1a87391838caf762d
Riza <riza.on@gmail.com>
2015-10-05 18:04:03 -0500

    put try-catch to message broadcasting

--- limbo/plugins/tg_broadcast.py
+++ limbo/plugins/tg_broadcast.py
@@ -1,7 +1,10 @@
 
 import re
+import logging
 
 def on_message(msg, server):
+    logger = logging.getLogger(__name__)
+
     cid = msg.get("channel", "")
     subtype = msg.get("subtype", "")
     
@@ -18,9 +21,18 @@ def on_message(msg, server):
     # replace @everyone with !broadcasst for uzzbot
     text = text.replace("<!everyone>","!broadcast")
     
-    for row in server.query("SELECT chat_id FROM tg_id"):
-        chat_id = row[0]
-        chat_text = text.encode('utf-8')
-        server.tg_bot.sendMessage(chat_id=chat_id, text=chat_text)
-    
+    rows = server.query("SELECT chat_id FROM tg_id")
+    failed = []
+    for row in rows:
+        try:
+            chat_id = row[0]
+            chat_text = text.encode('utf-8')
+            server.tg_bot.sendMessage(chat_id=chat_id, text=chat_text)
+        except Exception as e:
+            failed.append("%s: %s" (str(row[0]),str(e)))
+    retval = "Message broadcasted to %d chats: %s" % (len(rows), text)
+    if len(failed) > 0:
+        retval += "\n Following id are unable to contact: " + ",".join(failed)
+        logger.warning(retval)
+
     return
